ded203dd061dbb3d06d280b01f3d6aa57b87b41c
krontill <till.work@gmail.com>
2018-11-19 18:24:09 +0300

    Create maze.

--- src/maze/index.js
+++ src/maze/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './style.css';
+
 // За основу взяла статья https://habr.com/post/262345/
 
 class Maze extends Component {
@@ -8,8 +9,8 @@ class Maze extends Component {
 	static propTypes = {};
 
 	state = {
-		width: 7,
-		height: 7,
+		width: 45,
+		height: 45,
 		maze: []
 	};
 
@@ -27,28 +28,31 @@ class Maze extends Component {
 			}
 		}
 		this.generation(maze);
-		//this.randomRemoveWall(maze);
+		this.randomRemoveWall(maze);
 		this.setState({maze});
 	}
 
 	generation(maze) {
-		let startPoint = {x: 1, y: 1};
-		let currentCell = startPoint;
+		let currentCell = {x: 1, y: 1};
 		let mazeCellVisited = [];
+		mazeCellVisited.push(currentCell.x + '-' + currentCell.y);
 		let neighbourCell;
 		do {
-			let neighbours = this.getNeighbours(maze, mazeCellVisited, startPoint, 2);
-			console.log('neighbours: ', neighbours);
+			let neighbours = this.getNeighbours(maze, mazeCellVisited, currentCell, 2);
 			if (neighbours.length !== 0) { //если у клетки есть непосещенные соседи
 				neighbourCell = neighbours[Math.floor(Math.random() * neighbours.length)]; //выбираем случайного соседа
-				console.log('neighbourCell: ', neighbourCell);
-				mazeCellVisited.push(startPoint); //заносим текущую точку в стек
 				maze = this.removeWall(currentCell, neighbourCell, maze); //убираем стену между текущей и соседней точками
 				currentCell = neighbourCell; //делаем соседнюю точку текущей и отмечаем ее посещенной
+				mazeCellVisited.push(currentCell.x + '-' + currentCell.y); //заносим текущую точку в стек
 			}
-			/*else if (stackSize > 0) { //если нет соседей, возвращаемся на предыдущую точку
-				startPoint = pop();
-			}*/
+			else if (neighbours.length === 0) { //если нет соседей, возвращаемся на предыдущую точку
+				let indexcurrentCell = mazeCellVisited.indexOf(currentCell.x + '-' + currentCell.y);
+				if (~indexcurrentCell) { // Если у последнего currentCell нет соседей, то он еще не успеет попасть в mazeCellVisited.
+					currentCell = {x: +mazeCellVisited[indexcurrentCell - 1].split('-')[0], y: +mazeCellVisited[indexcurrentCell - 1].split('-')[1]};
+				} else {
+					currentCell = {x: +mazeCellVisited[mazeCellVisited.length - 1].split('-')[0], y: +mazeCellVisited[mazeCellVisited.length - 1].split('-')[1]};
+				}
+			}
 			else { //если нет соседей и точек в стеке, но не все точки посещены, выбираем случайную из непосещенных
 				/*cellString
 				cellStringUnvisited = getUnvisitedCells(width, height, maze);
@@ -56,11 +60,23 @@ class Maze extends Component {
 				currentCell = cellStringUnvisited.cells[randNum];
 				free(cellStringUnvisited.cells);*/
 			}
-		} while (0 > 0) ; //unvisitedCount()
+		} while (this.unvisitedCount(maze, mazeCellVisited) > 0);
+	}
+
+	unvisitedCount(maze, mazeCellVisited) {
+		let {width, height} = this.state;
+		let count = 0;
+		for (let i = 0; i < height; i++) {
+			for (let j = 0; j < width; j++) {
+				if (i % 2 && j % 2 && maze[i][j] !== 1 && !~mazeCellVisited.indexOf(i + '-' + j)) { //i % 2 && j % 2 изначально непосещенные
+					count++;
+				}
+			}
+		}
+		return count;
 	}
 
 	removeWall(currentCell, neighbourCell, maze) {
-		console.log('currentCell, neighbourCell, maze: ', currentCell, neighbourCell, maze);
 		let xDiff = neighbourCell.x - currentCell.x;
 		let yDiff = neighbourCell.y - currentCell.y;
 		let addX, addY;
@@ -84,14 +100,13 @@ class Maze extends Component {
 		let rt = {x: x + distance, y};
 		let dw = {x, y: y + distance};
 		let lt = {x: x - distance, y};
-		console.log('dw, rt, up, lt: ', dw, rt, up, lt);
 		let d = [];
 		d.push(dw, rt, up, lt);
 		let cellCurrentNeighbours = [];
 
 		for (let i = 0; i < 4; i++) { //для каждого направдения
 			if (d[i].x > 0 && d[i].x < width && d[i].y > 0 && d[i].y < height) { //если не выходит за границы лабиринта
-				if (maze[d[i].y][d[i].x] !== 1 && !~mazeCellVisited.indexOf(d[i])) { //и не посещена\является стеной  && mazeCellCurrent !== VISITED
+				if (maze[d[i].y][d[i].x] !== 1 && !~mazeCellVisited.indexOf(d[i].x + '-' + d[i].y)) { //и не посещена\является стеной  && mazeCellCurrent !== VISITED
 					cellCurrentNeighbours.push(d[i]);
 				}
 			}
@@ -102,10 +117,10 @@ class Maze extends Component {
 
 	randomRemoveWall(maze) {
 		const {width, height} = this.state;
-		for (let i = 0; i < height; i++) {
-			for (let j = 0; j < width; j++) {
+		for (let i = 1; i < height - 1; i++) {
+			for (let j = 1; j < width - 1; j++) {
 				if (maze[i][j] === 1) {
-					if (!(Math.random() * 10 ^ 0) % 5) {
+					if (!(Math.random() * 100 ^ 0) % 17) {
 						maze[i][j] = 0;
 					}
 				}
